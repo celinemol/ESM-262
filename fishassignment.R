f78bb41caa09a395d292698a38604f309b30e8b1
Celine Mol <celinemol@umail.ucsb.edu>
2018-06-02 11:36:03 -0700

    small comments

--- fishassignment.R
+++ fishassignment.R
@@ -16,7 +16,8 @@
 
 #* if user requests it graph of revenue by location and total revenue (as text)
 
-## Generate some example data for your function
+## Generate some example data for your function.
+# NOTE: This is going to go into a separate file later... just here for convenience
 
 # Fish prices
 fish = as.factor(c("Cod", "Salmon", "Steelhead", "Tuna", "Shark", "Sole"))
@@ -30,12 +31,17 @@ location2 = sample(fish, size=10, prob = c(0.4, 0.1, 0.2, 0.1, 0.2, 0.0), replac
 location3 = sample(fish, size=10, prob = c(0.1, 0.2, 0.3, 0.0, 0.0, 0.4), replace=T)
 fish_catches = as.data.frame(cbind(location1, location2, location3))
 
+## --- Function begins here
 
 fisheries = function(fish_price, fish_catches, showplot = FALSE) {
   
+  # data prep
+  fish_catches = as.data.frame(fish_catches)
+  nloc = ncol(fish_catches)
+  
   # * most frequently caught fish in each location
   mostfrequent = list()
-  for(i in 1:ncol(fish_catches)){
+  for(i in 1:nloc){
     catch = sprintf("In %s we mostly caught %s", 
             colnames(fish_catches)[i], 
             names(which.max(summary(as.factor(fish_catches[,i])))))
@@ -45,7 +51,7 @@ fisheries = function(fish_price, fish_catches, showplot = FALSE) {
   # * total revenue for each location
   location_names = colnames(fish_catches)
   colnames(fish_catches) = rep("fish", ncol(fish_catches))
-  #wholetable = join(fish_catches, fish_price, type = "full", 
+  #wholetable = left_join(fish_catches, fish_price, type = "full", 
                     #by = "fish")
   
   # * total fisheries revenue sum
